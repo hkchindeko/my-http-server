76f0cb92650d03098233a77b9381bdbecd0da5fe
Laurent Chindeko <laurent.chindeko.hk@gmail.com>
2023-01-16 16:49:36 +0800

    original code

--- src/main.rs
+++ src/main.rs
@@ -2,32 +2,39 @@ use std::net::SocketAddr;
 
 use hyper::server::conn::Http;
 use hyper::service::service_fn;
-use hyper::{Body, Method, Request, Response};
+use hyper::{Body, Method, Request, Response, StatusCode};
 use tokio::net::TcpListener;
 
-async fn router(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+/// This is our service handler. It receives a Request, routes on its
+/// path, and returns a Future of a Response.
+async fn echo(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
     match (req.method(), req.uri().path()) {
+        // Serve some instructions at /
         (&Method::GET, "/") => Ok(Response::new(Body::from(
-            "👋 Hello World 🌍",
+            "Try POSTing data to /echo such as: `curl localhost:8080/echo -XPOST -d 'hello world'`",
         ))),
 
-        // (&Method::POST, "/hello") => {
-        //     let name = hyper::body::to_bytes(req.into_body()).await?;
-        //     let name_string = String::from_utf8(name.to_vec()).unwrap();
+        // Simply echo the body back to the client.
+        (&Method::POST, "/echo") => Ok(Response::new(req.into_body())),
 
-        //     let answer = format!("{}{}", "👋 Hello ".to_owned(), name_string);
+        (&Method::POST, "/echo/reversed") => {
+            let whole_body = hyper::body::to_bytes(req.into_body()).await?;
 
-        //     Ok(Response::new(Body::from(answer)))
-        // }
+            let reversed_body = whole_body.iter().rev().cloned().collect::<Vec<u8>>();
+            Ok(Response::new(Body::from(reversed_body)))
+        }
 
+        // Return the 404 Not Found for other routes.
         _ => {
-            Ok(Response::new(Body::from("😡 try again")))
+            let mut not_found = Response::default();
+            *not_found.status_mut() = StatusCode::NOT_FOUND;
+            Ok(not_found)
         }
     }
 }
 
 #[tokio::main(flavor = "current_thread")]
-async fn main() -> Result<(), Box<dyn std::error::Error + Send +Sync>> {
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
 
     let listener = TcpListener::bind(addr).await?;
@@ -36,7 +43,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error + Send +Sync>> {
         let (stream, _) = listener.accept().await?;
 
         tokio::task::spawn(async move {
-            if let Err(err) = Http::new().serve_connection(stream, service_fn(router)).await {
+            if let Err(err) = Http::new().serve_connection(stream, service_fn(echo)).await {
                 println!("Error serving connection: {:?}", err);
             }
         });
